joyguoguo/oss-fuzz-py2

commit 14588286bdee4fea33cfbf2c3fafd17b2aefca26

mpv: add support for Rust (#13137)
- Switch to base-builder-rust
- Create venv for pip installed packages
- Set rustc target explicity, needed for i386 build
- Don't fail if subprojects directory already exists
- Don't compress Matroska files to speed zip creation, they are not
really compressible anyway.

projects/mpv/Dockerfile
@@ -12,9 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM gcr.io/oss-fuzz-base/base-builder
-RUN apt-get update && apt-get install -y nasm pkg-config gperf zip rsync && pip3 install meson ninja
+FROM gcr.io/oss-fuzz-base/base-builder-rust
+
+RUN <<EOT
+    set -ex
+
+    apt-get update
+    apt-get install --no-install-recommends -y nasm pkgconf gperf zip rsync
+    apt-get clean -y && rm -rf /var/lib/apt/lists/*
+EOT
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV VIRTUAL_ENV=$SRC/venv
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+RUN <<EOT
+    set -ex
+
+    python -m venv $VIRTUAL_ENV
+    python -m pip --no-cache-dir install meson ninja
+EOT
+
 RUN git clone --depth 1 https://github.com/mpv-player/mpv mpv
 RUN git clone --depth 1 https://github.com/FFmpeg/FFmpeg ffmpeg
-WORKDIR mpv
+
+WORKDIR $SRC/mpv
 COPY build.sh $SRC/

projects/mpv/build.sh
@@ -17,9 +17,13 @@ if [[ "$ARCHITECTURE" == i386 ]]; then
   export PKG_CONFIG_PATH=/usr/local/lib/i386-linux-gnu/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig
   LIBDIR='lib/i386-linux-gnu'
   FFMPEG_BUILD_ARGS='--arch="i386" --cpu="i386" --disable-inline-asm'
+  RUST_TARGET='i686-unknown-linux-gnu'
+  rustup target add $RUST_TARGET
 else
   LIBDIR='lib/x86_64-linux-gnu'
   FFMPEG_BUILD_ARGS=''
+  RUST_TARGET='x86_64-unknown-linux-gnu'
+  rustup target add $RUST_TARGET
 fi
 
 export FUZZ_INTROSPECTOR_CONFIG=$SRC/fuzz_introspector_exclusion.config
@@ -58,7 +62,7 @@ pushd $SRC/mpv
 sed -i -e "/^\s*flags += \['-fsanitize=address,undefined,fuzzer', '-fno-omit-frame-pointer'\]/d; \
           s|^\s*link_flags += \['-fsanitize=address,undefined,fuzzer', '-fno-omit-frame-pointer'\]| \
           link_flags += \['$LIB_FUZZING_ENGINE'\]|" meson.build
-mkdir subprojects
+mkdir subprojects -p
 meson wrap install expat
 meson wrap install fontconfig
 meson wrap install freetype2
@@ -91,10 +95,10 @@ meson setup build -Dbackend_max_links=4 -Ddefault_library=static -Dprefer_static
                   -Dlcms2:jpeg=disabled -Dlcms2:tiff=disabled \
                   -Dlibass:fontconfig=enabled -Dlibass:asm=disabled \
                   -Dc_link_args="$CXXFLAGS -lc++" -Dcpp_link_args="$CXXFLAGS" \
-                  --libdir $LIBDIR
+                  --libdir $LIBDIR -Drust_args="--target=$RUST_TARGET"
 meson compile -C build fuzzers
 
 find ./build/fuzzers -maxdepth 1 -type f -name 'fuzzer_*' -exec mv {} "$OUT" \; -exec echo "{} -> $OUT" \;
 
-rsync -av rsync://samples.ffmpeg.org/samples/Matroska $SRC/matroska
-zip -r $OUT/fuzzer_loadfile_mkv_seed_corpus.zip $SRC/matroska -i '*.mkv' '*.mka'
+rsync --no-compress -av rsync://samples.ffmpeg.org/samples/Matroska $SRC/matroska
+zip -0 -r $OUT/fuzzer_loadfile_mkv_seed_corpus.zip $SRC/matroska -i '*.mkv' '*.mka'

commit 13ea66b2724e281d33be7464e1ada88f4bb5ad31

mpv: Don't use heredoc in Dockerfile (#13154)
Apparently, the Docker version on build machines is older and doesn't
support heredoc. It differs from the ones tested on CI before the merge.

Also, set RUSTC directly with the target because Meson doesn't append
rust_args in all invocations. This is mostly a cosmetic change, as it
was already appended where needed in our case.

Fixes: 14588286bdee4fea33cfbf2c3fafd17b2aefca26

projects/mpv/Dockerfile
@@ -14,23 +14,16 @@
 
 FROM gcr.io/oss-fuzz-base/base-builder-rust
 
-RUN <<EOT
-    set -ex
-
-    apt-get update
-    apt-get install --no-install-recommends -y nasm pkgconf gperf zip rsync
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y nasm pkgconf gperf zip rsync && \
     apt-get clean -y && rm -rf /var/lib/apt/lists/*
-EOT
 
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV VIRTUAL_ENV=$SRC/venv
 ENV PATH="$VIRTUAL_ENV/bin:$PATH"
-RUN <<EOT
-    set -ex
 
-    python -m venv $VIRTUAL_ENV
+RUN python -m venv $VIRTUAL_ENV && \
     python -m pip --no-cache-dir install meson ninja
-EOT
 
 RUN git clone --depth 1 https://github.com/mpv-player/mpv mpv
 RUN git clone --depth 1 https://github.com/FFmpeg/FFmpeg ffmpeg

projects/mpv/build.sh
@@ -26,6 +26,8 @@ else
   rustup target add $RUST_TARGET
 fi
 
+export RUSTC="rustc --target=$RUST_TARGET"
+
 export FUZZ_INTROSPECTOR_CONFIG=$SRC/fuzz_introspector_exclusion.config
 cat > $FUZZ_INTROSPECTOR_CONFIG <<EOF
 FILES_TO_AVOID
@@ -95,7 +97,7 @@ meson setup build -Dbackend_max_links=4 -Ddefault_library=static -Dprefer_static
                   -Dlcms2:jpeg=disabled -Dlcms2:tiff=disabled \
                   -Dlibass:fontconfig=enabled -Dlibass:asm=disabled \
                   -Dc_link_args="$CXXFLAGS -lc++" -Dcpp_link_args="$CXXFLAGS" \
-                  --libdir $LIBDIR -Drust_args="--target=$RUST_TARGET"
+                  --libdir $LIBDIR
 meson compile -C build fuzzers
 
 find ./build/fuzzers -maxdepth 1 -type f -name 'fuzzer_*' -exec mv {} "$OUT" \; -exec echo "{} -> $OUT" \;